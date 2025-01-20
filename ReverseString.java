4adfb622b4d4fc5462c177d9421faa8114127ffd
Chanchal-Ratnani <ratnanichanchal@gmail.com>
2025-01-20 23:35:23 +0500

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "Hello, World!";
+        String reversed = reverseString(input);
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            stack.push(ch);
+        }
+
+        StringBuilder reversed = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+
+        return reversed.toString();
+    }
+}
