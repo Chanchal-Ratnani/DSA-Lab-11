b0d47db7645fab6db98ee46e594609a3831f1c03
Chanchal-Ratnani <ratnanichanchal@gmail.com>
2025-01-20 23:38:02 +0500

    Add files via upload

---
+++ RemoveDuplicatesSLL .java
@@ -0,0 +1,69 @@
+class RemoveDuplicatesSLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void removeDuplicates() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesSLL list = new RemoveDuplicatesSLL();
+
+        list.addNode(10);
+        list.addNode(10);
+        list.addNode(20);
+        list.addNode(30);
+        list.addNode(30);
+        list.addNode(30);
+        list.addNode(40);
+
+        System.out.println("Original list:");
+        list.printList();
+
+        list.removeDuplicates();
+
+        System.out.println("List after removing duplicates:");
+        list.printList();
+    }
+}
